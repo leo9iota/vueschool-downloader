85f25a3a3010a35e2f209825a6d829cd086b460d
leo9iota <leo9iota@gmail.com>
2025-03-01 14:00:16 +0100

    Add Playwright example for `vueschool.io`

--- index.ts
+++ index.ts
@@ -1,1 +1,13 @@
-console.log("Hello via Bun!");
+import { chromium } from "playwright";
+
+async function run() {
+  const browser = await chromium.launch({ headless: true });
+  const page = await browser.newPage();
+  await page.goto("https://vueschool.io");
+
+  console.log("Title:", await page.title());
+
+  await browser.close();
+}
+
+run().catch(console.error);
