000a1facfa9f97f29f4c152845fb8d840e323ee5
leo9iota <leo9iota@gmail.com>
2025-03-01 16:23:30 +0100

    Add explicit types

--- index.ts
+++ index.ts
@@ -1,19 +1,20 @@
+import { Browser } from 'playwright';
 import { launchBrowser } from './src/browser';
 import { createNewPage } from './src/scraper';
 import { getCourseUrls } from './src/courses';
 import { saveToFile } from './src/utils';
 
-(async () => {
-    const browser = await launchBrowser();
+(async (): Promise<void> => {
+    const browser: Browser = await launchBrowser();
     const page = await createNewPage(browser);
 
     try {
-        const courseUrls = await getCourseUrls(page);
+        const courseUrls: string[] = await getCourseUrls(page);
         console.log('Extracted Course URLs:', courseUrls);
 
         // Save results to a JSON file
         await saveToFile('./output/courses.json', courseUrls);
-    } catch (error) {
+    } catch (error: unknown) {
         console.error('❌ Error scraping courses:', error);
     } finally {
         await browser.close();
